3a739bf245ef190c59a548de7049e68b36024df3
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2023-08-15 23:58:03 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 5.63 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        char[] arr = my_string.toCharArray();
+        char temp;
+        
+        for (int j = 0; j < queries.length; j++){
+            for (int i = queries[j][0]; i <= (queries[j][0] + queries[j][1]) / 2; i++) {
+                temp = arr[i];
+                arr[i] = arr[queries[j][0] + queries[j][1] - i];
+                arr[queries[j][0] + queries[j][1] - i] = temp;
+            }
+        }
+        return String.valueOf(arr);
+    }
+}
+
