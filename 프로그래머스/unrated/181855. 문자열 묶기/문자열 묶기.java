7b1f4ea37d8696d6296ec6260ab152de9526b1ef
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2023-10-05 09:15:18 +0900

    [unrated] Title: 문자열 묶기, Time: 25.05 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[] strArr) {
+        Map<Integer, Integer> groupCounts = new HashMap<>();
+        
+        for (String str : strArr) {
+            int length = str.length();
+            groupCounts.put(length, groupCounts.getOrDefault(length, 0) + 1);
+        }
+        
+        int maxCount = 0;
+        
+        for (int count : groupCounts.values()) {
+            if (count > maxCount) {
+                maxCount = count;
+            }
+        }
+        
+        return maxCount;
+    }
+}
