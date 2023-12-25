4a4f9190d8d49ce3702f99230a863aaa96097aab
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2023-12-25 23:36:34 +0900

    [Bronze III] Title: 더하기, Time: 200 ms, Memory: 17252 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int sum = 0;
+            
+            for (int j = 0; j < a; j++) {
+                sum += sc.nextInt();
+            }
+            System.out.println(sum);
+        }
+    }
+}
