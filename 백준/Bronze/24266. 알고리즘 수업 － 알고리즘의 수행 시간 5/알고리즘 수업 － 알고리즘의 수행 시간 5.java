c74127bf7aafdd09edc327a39118303bf73df325
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2024-02-10 23:35:54 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long N = sc.nextLong();
+
+        System.out.println(N * N * N);
+        System.out.println(3);
+    }
+}
