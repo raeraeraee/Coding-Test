38e7897194e9d1a3debf33e7da3402fcf588c37e
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2024-02-12 23:55:41 +0900

    [Silver V] Title: 막대기, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int count = 0;
+        int stick = 64;
+
+		while (X > 0) {
+			if (stick > X) {
+				stick /= 2;
+			} else {
+                count++;
+				X -= stick;	
+			}
+		}
+		System.out.println(count);
+	}
+}
