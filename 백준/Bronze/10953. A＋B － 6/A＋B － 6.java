78c8ea534eda775449c28ffcf3208ea55abe39b4
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2024-02-19 23:15:21 +0900

    [Bronze III] Title: A+B - 6, Time: 228 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            String[] s = sc.next().split(",");
+            int A = Integer.parseInt(s[0]);
+            int B = Integer.parseInt(s[1]);
+            
+            System.out.println(A + B);
+        }
+    }
+}
