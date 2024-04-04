b5e0d0bf609e5b0c0c290d70e5e0fe19e22b983a
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2024-04-05 01:59:30 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String st = sc.next();
-		int N = (int)st.charAt(0);
-		
-		System.out.println(N);
+	public static void main(String[] args) throws Exception {
+ 
+		int a = System.in.read();
+		System.out.print(a);
 	}
-
 }
