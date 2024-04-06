38bb43d41c451098c956a855ff4f9259e4c4a57f
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2024-04-06 23:25:20 +0900

    [Bronze III] Title: 다면체, Time: 204 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int i = 0; i < T; i++) {
+			int V = sc.nextInt();
+			int E = sc.nextInt();
+			int S = E - V + 2;
+			System.out.println(S);
+		}
+	}
+}
