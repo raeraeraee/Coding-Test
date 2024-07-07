13c69bf300f83b64b2b54cf00f010c238edb15fd
Seonrae Kim <134375389+raeraeraee@users.noreply.github.com>
2024-07-07 21:48:37 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 212 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);        
+        long x = sc.nextLong();
+        long y = sc.nextLong();
+        
+        System.out.println(x * y / 2);
+    }
+}
